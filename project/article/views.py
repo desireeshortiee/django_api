2d15e3b4f71481b8dba37e8ee16ac749f165419c
desireeshortiee <desireeganohay13@gmail.com>
2025-03-24 09:04:56 +0800

    first commit

--- project/article/views.py
+++ project/article/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Article, Comment
+from rest_framework import generics
+from .models import Article, Comment
 from .serializers import ArticleSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+class ArticleListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+
+class ArticleRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+class CommentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
 
 # Create your views here.
 class ArticleListCreateAPIView(generics.ListCreateAPIView):
